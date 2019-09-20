12fbb85889d1ba0837ae97495cc2536d5ce2216d
kooky-waterfall-9790 <akimclerge@gmail.com>
2019-09-20 18:16:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,13 @@
 # Add your variables here
+<<<<<<< HEAD
 first_number = 6  
 second_number= 3 
 sum = 9
 difference = 3
 product = 18
 quotient = 2
+=======
+first_number = 1  
+second_number= 1 
+sum = 2 
+>>>>>>> 4e7b24773d409d41d6469c7a8c3c1639d1c81159
